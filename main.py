56d2d0fe71d11f8fb26697d299862992c159a2f2
kronic5179 <98897386+Allen-Saunders@users.noreply.github.com>
2022-10-18 17:31:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+name = input("please enter your name.")
+print("Welcome " + name + ".")
+age = int(input("How old are you? "))
+age = age + 10
+print(name + ", you will be " + str(age) + " years old in 10 years.")
